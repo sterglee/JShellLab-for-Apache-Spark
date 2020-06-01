a3fe9da3ee48a9edbbf85890c3ba03c1efd62a92
sterg <sterg@teiemt.gr>
2020-06-01 14:37:15 +0300

    minor update

---
+++ src/org/ejml/EjmlVersion.java
@@ -0,0 +1,19 @@
+package org.ejml;
+
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class EjmlVersion {
+	public static final String MAVEN_GROUP = "org.ejml";
+	public static final String MAVEN_NAME = "ejml";
+	public static final String VERSION = "0.39";
+	public static final int GIT_REVISION = 795;
+	public static final String GIT_SHA = "84f2e863364370c6acb0233bb92dbb83dd37d76b";
+	public static final String GIT_DATE = "2020-04-07T01:08:56Z";
+	public static final String BUILD_DATE = "2020-04-07T01:12:37Z";
+	public static final long BUILD_UNIX_TIME = 1586221957094L;
+	public static final int DIRTY = 0;
+
+	private EjmlVersion(){}
+}
